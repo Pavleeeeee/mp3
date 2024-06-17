8f960d93a2463d935cb2e65b23cf2962c0889557
Pavleeeeee <149153830+Pavleeeeee@users.noreply.github.com>
2024-06-17 21:23:19 +0200

    mp3.py

--- mp3.py
+++ mp3.py
@@ -69,7 +69,7 @@ listbox_frame = LabelFrame(root, text='Playlist', bg='RoyalBlue')
 listbox_frame.place(x=700, y=0, height=200, width=300)
 
 ime_pesme = StringVar(root, value='<Not selected>')
-song_status = StringVar(root, value='<Not Available>')
+status_pesme = StringVar(root, value='<Not Available>')
 
 playlist = Listbox(listbox_frame, font=('Helvetica', 11), selectbackground='Gold', bg='#3A3A3A', fg='white', selectmode=SINGLE)
 playlist.pack(fill=BOTH, padx=5, pady=5, expand=True)
@@ -86,31 +86,31 @@ Label(song_frame, text='Trenutna pesma:', bg='RoyalBlue', font=('Times', 10, 'bo
 song_lbl = Label(song_frame, textvariable=ime_pesme, bg='Goldenrod', font=("Times", 12), width=40)
 song_lbl.place(x=150, y=20)
 
-pause_btn = Button(button_frame, text='Pause', bg='Aqua', font=("Georgia", 13), width=7, command=lambda: pauza(song_status))
+pause_btn = Button(button_frame, text='Pause', bg='Aqua', font=("Georgia", 13), width=7, command=lambda: pauza(status_pesme))
 pause_btn.place(x=15, y=10)
 
-stop_btn = Button(button_frame, text='Stop', bg='Aqua', font=("Georgia", 13), width=7, command=lambda: zaustavi(song_status))
+stop_btn = Button(button_frame, text='Stop', bg='Aqua', font=("Georgia", 13), width=7, command=lambda: zaustavi(status_pesme))
 stop_btn.place(x=100, y=10)
 
-play_btn = Button(button_frame, text='Play', bg='Aqua', font=("Georgia", 13), width=7, command=lambda: pusti(ime_pesme, playlist, song_status))
+play_btn = Button(button_frame, text='Play', bg='Aqua', font=("Georgia", 13), width=7, command=lambda: pusti(ime_pesme, playlist, status_pesme))
 play_btn.place(x=185, y=10)
 
-resume_btn = Button(button_frame, text='Resume', bg='Aqua', font=("Georgia", 13), width=7, command=lambda: resume(song_status))
+resume_btn = Button(button_frame, text='Resume', bg='Aqua', font=("Georgia", 13), width=7, command=lambda: resume(status_pesme))
 resume_btn.place(x=270, y=10)
 
-skip_btn = Button(button_frame, text='Next', bg='Aqua', font=("Georgia", 13), width=10, command=lambda: preskoci(song_status, playlist))
+skip_btn = Button(button_frame, text='Next', bg='Aqua', font=("Georgia", 13), width=10, command=lambda: preskoci(status_pesme, playlist))
 skip_btn.place(x=355, y=10)
 
-previous_btn = Button(button_frame, text='Previous', bg='Aqua', font=("Georgia", 13), width=10, command=lambda: prethodna(song_status, playlist))
+previous_btn = Button(button_frame, text='Previous', bg='Aqua', font=("Georgia", 13), width=10, command=lambda: prethodna(status_pesme, playlist))
 previous_btn.place(x=470, y=10)
 
-shuffle_btn = Button(button_frame, text='Shuffle', bg='Aqua', font=("Georgia", 13), width=10, command=lambda: shuffle(ime_pesme, playlist, song_status))
+shuffle_btn = Button(button_frame, text='Shuffle', bg='Aqua', font=("Georgia", 13), width=10, command=lambda: shuffle(ime_pesme, playlist, status_pesme))
 shuffle_btn.place(x=585, y=10)
 
 load_btn = Button(button_frame, text='Load Directory', bg='Aqua', font=("Georgia", 13), width=68, command=lambda: load(playlist))
 load_btn.place(x=10, y=55)
 
-Label(root, textvariable=song_status, bg='SteelBlue', font=('Times', 9), justify=LEFT).pack(side=BOTTOM, fill=X)
+Label(root, textvariable=status_pesme, bg='SteelBlue', font=('Times', 9), justify=LEFT).pack(side=BOTTOM, fill=X)
 
 root.update()
 root.mainloop()
